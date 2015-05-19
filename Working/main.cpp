7e13d37e65abab3dd8093d4e837cec4861730b77
anthonywittemann <awittema@usc.edu>
2015-05-18 22:21:44 -0700

    Initial Commit

---
+++ Working/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Working
+//
+//  Created by Anthony Wittemann on 5/18/15.
+//  Copyright (c) 2015 Anthony Wittemann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
