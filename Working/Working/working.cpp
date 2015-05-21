de50fad3aebfd6a8477c45c19d419ed9b1e81a04
anthonywittemann <awittema@usc.edu>
2015-05-21 10:20:37 -0700

    using variables from the preprocessor header files

--- Working/Working/working.cpp
+++ Working/Working/working.cpp
@@ -1,13 +1,15 @@
 // working.cpp by Bill Weinman <http://bw.org/>
 #include <cstdio>
+#include "preproc.h"
 using namespace std;
 
-#define ONE 1 //not a variable -- cannot get it's address, add a pointer to it
+//#define ONE 1 //not a variable -- cannot get it's address, add a pointer to it
                 // it replaces ONE with the literal value of 1
 
 int main( int argc, char ** argv )
 {
     //puts("Hello, World!");
     printf("ONE: %d\n", ONE);
+    printf("preprocessor macro: %s\n", _sOne); //_sOne is defined in preproc.h
     return 0;
 }
