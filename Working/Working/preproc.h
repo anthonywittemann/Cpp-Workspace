14a865f7a57ad0dc713714693bbb988d74380625
anthonywittemann <awittema@usc.edu>
2015-05-21 10:56:14 -0700

    including files only once ifndef

--- Working/Working/preproc.h
+++ Working/Working/preproc.h
@@ -1,6 +1,6 @@
 // preproc.h by Bill Weinman <http://bw.org/>
 
-#ifndef PREPROC_H_
+#ifndef PREPROC_H_      //used to make sure that this file is not defined multiple times by other files that may use it
 #define PREPROC_H_
 
 #define ONE 1
@@ -8,4 +8,5 @@
 const int _iOne = 1;
 const char _sOne[] = "one";
 
-#endif /* PREPROC_H_ */
+#endif /* PREPROC_H_ */     
+
