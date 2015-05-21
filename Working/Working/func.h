71545ee1f2a1b7a13dfa4c0743b451d2da9aa855
anthonywittemann <awittema@usc.edu>
2015-05-20 18:11:04 -0700

    jump - switch statement for functions

--- Working/Working/func.h
+++
@@ -1,9 +0,0 @@
-// func.h by Bill Weinman <http://bw.org/>
-
-#ifndef FUNC_H_
-#define FUNC_H_
-
-void func();
-void ch1Func(); //since the function is declared here, I can define it anywhere in the code
-
-#endif // FUNC_H_
