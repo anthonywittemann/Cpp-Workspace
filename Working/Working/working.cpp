4f6c19d478ae768828e70cca708644455abbedf7
anthonywittemann <awittema@usc.edu>
2015-05-21 12:37:27 -0700

    structs and pointers

--- Working/Working/working.cpp
+++ Working/Working/working.cpp
@@ -1,36 +1,23 @@
 // working.cpp by Bill Weinman <http://bw.org/>
 #include <cstdio>
-#include "preproc.h"
 using namespace std;
 
-//#define ONE 1 //not a variable -- cannot get it's address, add a pointer to it
-                // it replaces ONE with the literal value of 1
 
-//defining more powerful macros
-#define MACRO(a, b) (a * b)
-#define MAX(a, b) (a > b ? a : b)
-
-//line continuation -- ^= is XOR
-#define SWAP(a, b) do { \
-                        a ^= b;   \
-                        b ^= a;   \
-                        a ^= b;   \
-                    }while(0)
+struct A{
+    int ia;
+    int ib;
+    int ic;
+};
 
 int main( int argc, char ** argv )
 {
-    puts("Hel\
-         lo, World!"); //line continutation with \ and new line
-    printf("ONE: %d\n", ONE);
-    printf("preprocessor macro: %s\n", _sOne); //_sOne is defined in preproc.h
+    //accessing data members using structs -- public access?
+    A a = {1, 2, 3};
+    printf("ia is %d, ib is %d, ic is %d\n", a.ia, a.ib, a.ic);
     
-    int ia = 5;
-    int ib = 6;
-    printf("Macro product: %d\n", MACRO(ia, ib)); //MACRO(ia, ib) is equivalent to (ia * ib)
-    printf("Max: %d\n", MAX(ia, ib));
+    //accessing data members using pointers to structs
+    A *pa = &a;
+    printf("ia is %d, ib is %d, ic is %d\n", pa->ia, pa->ib, pa->ic); //-> is pointer selector operator
     
-    printf("ia is %d, ib is %d\n", ia, ib);
-    SWAP(ia, ib);
-    printf("ia is %d, ib is %d\n", ia, ib);
     return 0;
 }
