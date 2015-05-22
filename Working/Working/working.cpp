b7df10ad1b6985954aef3e5b940e6c856e792e51
anthonywittemann <awittema@usc.edu>
2015-05-22 15:44:53 -0700

    cpp integer types

---
+++ Working/Working/working.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+
+int main( int argc, char ** argv )
+{
+    //_____________________integer types_______________________
+    
+    //char - 8 bits
+    //short int - 16 bits, signed or unsigned
+    //int - 32 bits, signed or unsigned
+    //long int - 32 or 64 bits, signed or unsigned
+    //long long int - 64 bits, signed or unsigned
+    
+    puts("Hello, World!");
+    return 0;
+}
