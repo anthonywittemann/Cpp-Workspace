047251da4c267ed4e49b0730298d1e197002576e
anthonywittemann <awittema@usc.edu>
2015-05-22 16:00:39 -0700

    more notes and examples on integer types in cpp

--- Working/Working/working.cpp
+++ Working/Working/working.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdint> //for standardizing integer lengths on different OSs
 using namespace std;
 
 int main( int argc, char ** argv )
@@ -6,11 +7,27 @@ int main( int argc, char ** argv )
     //_____________________integer types_______________________
     
     //char - 8 bits
+    printf("The size of a char is %ld bytes \n", sizeof(char));
     //short int - 16 bits, signed or unsigned
+    printf("The size of a short int is %ld bytes \n", sizeof(short int));
     //int - 32 bits, signed or unsigned
+    printf("The size of an int is %ld bytes \n", sizeof(int));
     //long int - 32 or 64 bits, signed or unsigned
+    printf("The size of a long int is %ld bytes \n", sizeof(long int));
     //long long int - 64 bits, signed or unsigned
+    printf("The size of a long long int is %ld bytes \n", sizeof(long long int));
     
-    puts("Hello, World!");
+    
+    printf("The size of an int_8 is %ld bytes \n", sizeof(int8_t));
+    printf("The size of an int_16 is %ld bytes \n", sizeof(int16_t));
+    printf("The size of an int_32 int is %ld bytes \n", sizeof(int32_t));
+    printf("The size of an int_64 is %ld bytes \n", sizeof(int64_t));
+    
+    //different ints
+    long int x = 42LL;//long long int
+    x = 0xff93;//hexadecimal
+    x = 0223; //octal
+    x = 42U; //unsigned
+    x = 0b1001001; //binary
     return 0;
 }
